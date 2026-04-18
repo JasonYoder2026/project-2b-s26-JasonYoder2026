48b576226a3755f667658d2a70c403f92f3cc064
Jason Yoder <jason.yoder2@bsu.edu>
2026-04-18 11:36:20 -0400

    adds css

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,6 +1,5 @@
 DROP TABLE IF EXISTS destinations;
 
--- TODO: You will need to modify the fields here
 CREATE TABLE destinations (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
